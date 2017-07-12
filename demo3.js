a00296ce9c50c08cb875b967b8c3f33e54972185
justinmzt <294570635@qq.com>
2017-07-12 16:11:21 +0800

    Update demo3.js

--- demo3.js
+++ demo3.js
@@ -167,8 +167,7 @@ Game.prototype.display = function() {
 	this.ctx.font="20px Oswald, sans-serif";
 	this.ctx.fillText("Score : "+ this.score, 10, 25);
 	this.ctx.fillText("Max Score : "+this.maxScore, 10, 50);
-	this.ctx.fillText("Generation : "+this.generation, 10, 75);
-	this.ctx.fillText("Alive : "+this.alives+" / "+Neuvol.options.population, 10, 100);
+
     
     var self = this;
     requestAnimationFrame(function () {
